82d451336187dbb599c10257d5e4714b2e98f3df
türkay ürkmez <turkay.urkmez@outlook.com>
2024-05-21 10:06:29 +0300

    How to protected data in server?

---
+++ DataProtectionOnServer/DataProtectionOnServer/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using DataProtectionOnServer.Models;
+using DataProtectionOnServer.Security;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DataProtectionOnServer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            /*Sunucuya güvenme! Kritik verileri şifrele.... */
+            string value = "Bu cümle bizim için çok önemli";
+            DataProtector protector = new DataProtector("crypt.txt");
+            var encryptedLength = protector.EncryptData(value);
+            var decryptedValue = protector.DecryptData(encryptedLength);
+
+            ViewBag.Value = decryptedValue;
+
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DataProtectionOnServer/DataProtectionOnServer/Security/DataProtector.cs
@@ -0,0 +1,69 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace DataProtectionOnServer.Security
+{
+    public class DataProtector
+    {
+        /*
+         * Encode: Kod formatını değiştir (64 bit string))
+         * Encryption: Şifrele
+         * 
+         * Verilen adreste bir dosya oluşturan
+         * ve değeri, şifreleyerek bu dosyaya yazan 
+         * bir nesne yapıyoruz
+         * 
+         */
+
+        private readonly string path;
+        private readonly byte[] entropy;
+
+        public DataProtector(string path)
+        {
+            this.path = path;
+            entropy = RandomNumberGenerator.GetBytes(16);
+        }
+
+        public int EncryptData(string value)
+        {           
+            var encodedData = Encoding.UTF8.GetBytes(value);
+            using var fileStream = new FileStream(path, FileMode.OpenOrCreate);
+            var length = encryptDataToFile(encodedData, entropy, DataProtectionScope.CurrentUser, fileStream);
+            return length;
+        }
+
+        private int encryptDataToFile(byte[] value, byte[] entropy, DataProtectionScope protectionScope, FileStream fileStream)
+        {
+          var encrypted =  ProtectedData.Protect(value, entropy, protectionScope);
+
+            if (fileStream.CanWrite && encrypted != null)
+            {
+                fileStream.Write(encrypted, 0, encrypted.Length);
+            }
+            
+         return encrypted.Length;
+
+        }
+
+        public string DecryptData(int length)
+        {
+            FileStream fileStream = new FileStream(path, FileMode.Open);
+            byte[] decrypted = decryptDataFromFile(length,entropy, DataProtectionScope.CurrentUser, fileStream);
+
+            return Encoding.UTF8.GetString(decrypted);
+        }
+
+        private byte[] decryptDataFromFile(int length, byte[] entropy, DataProtectionScope scope, FileStream fileStream )        {
+            var input = new byte[length];
+            var output = new byte[length];          
+
+            if (fileStream.CanRead)
+            {
+                fileStream.Read(input, 0, input.Length);
+                output = ProtectedData.Unprotect(input, entropy, scope);
+            }
+            return output;
+        } 
+
+    }
+}
